4c69432374ea130f9acf36cb734a796069601c05
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-11 16:57:48 +0100

    array's done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,33 @@ var app = new Vue(
         el: '#root',
 
         data: {
-
+            photos: [
+                {
+                    name: 'imageNumber0',
+                    image: './../img/pexels-eugene-shelestov-33930.jpg',
+                    description: '',
+                },
+                {
+                    name: 'imageNumber1',
+                    image: './../img/pexels-kelly-2544992.jpg',
+                    description: '',
+                },
+                {
+                    name: 'imageNumber2',
+                    image: './../img/pexels-pixabay-220129.jpg',
+                    description: '',
+                },
+                {
+                    name: 'imageNumber3',
+                    image: './../img/pexels-sebastian-sørensen-750225.jpg',
+                    description: '',
+                },
+                {
+                    name: 'imageNumber4',
+                    image: './../img/pexels-skitterphoto-9334.jpg',
+                    description: '',
+                },        
+            ],
         },
 
         methods: {
